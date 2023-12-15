6169827a26b948db969f76a90994a0daba531fa7
Duarte Silva <duartemcsilva@gmail.com>
2023-12-15 12:14:41 +0000

    Deleted unnecessary files

--- app/blog/[postId]/page.js
+++
@@ -1,8 +0,0 @@
-export default function BlogPostPage({ params }) {
-    return (
-        <main>
-            <h1> Blog Post 1 </h1>
-            <p> {params.postId} </p>
-        </main>
-    );
-}
