e186a63c8a1dc52bcee62c731fadc8a90c1f4150
Duarte Silva <duartemcsilva@gmail.com>
2023-12-18 14:57:21 +0000

    Meal details page, and query by id

---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+export default function MealNotFound() {
+    return (
+        <main className="not-found">
+            <h1> Meal not found!</h1>
+            <p> Unfortunately, we could not find the requested meal data. </p>
+        </main>
+    );
+}
+
