f2ecbe7e3949ba9fb99590a787a70cd331351e0b
Duarte Silva <duartemcsilva@gmail.com>
2023-12-18 13:49:14 +0000

    Set db, loading page and error page

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+'use client';
+export default function Error({}) {
+    return (
+        <main className="error">
+           <h1>There was an error!</h1>
+        </main>
+    );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,37 @@
-export default function Meals() {
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css'
+import Link from 'next/link';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+import MealsLoadingPage from './loading-out';
+
+async function Meals() {
+    const meals = await getMeals();
+    return (<MealsGrid meals={meals}/>)
+
+}
+
+export default function MealsPage() {
     return (
-        <div>
-            Meals
-        </div>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created {'  '} <span className={classes.highlight}> by you</span>
+                </h1>
+                <p>
+                    Choose your recipe... try not the burn the kitchen
+                </p>
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main>
+                <Suspense fallback={<MealsLoadingPage/>}>
+                    <Meals/>
+                </Suspense>
+            </main>
+        </>
     );
 }
